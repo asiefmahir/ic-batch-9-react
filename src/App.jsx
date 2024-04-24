25ced5c522e66999745f7ed431aaf478fb66279c
Asief Mahir <asiefmahir1@gmail.com>
2024-04-24 22:46:10 +0600

    feat: Do Create and Remove functionalities using State

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,82 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	// Re-Render
-	console.log("I am being invoked");
-	// State Management
-	// State Declaration / State Initialization
-	// let a = 100;
-	const [dynamicCounter, setDynamicCounter] = useState(20);
-	// console.log(setDynamicCounter, "habijabi");
-	// console.log(dynamicCounter, "dc");
-	// dynamicCounter = dynamicCounter + 100
+	const [noteTitle, setNoteTitle] = useState("");
+	const [notes, setNotes] = useState([
+		{ id: "1", title: "Note 1" },
+		{ id: "2", title: "Note 2" },
+		{ id: "3", title: "Note 3" },
+	]);
 
-	const increaseHandler = () => {
-		setDynamicCounter(dynamicCounter + 1);
-		// setDynamicCounter === 21 + 1 === 22
-		// dynamicCounter = 21
+	const submitHandler = (event) => {
+		event.preventDefault();
+		if (noteTitle.trim() === "") return alert("Please enter valid a title");
+		const newNote = {
+			id: Date.now() + "",
+			title: noteTitle,
+		};
+		// notes.push(newNote);
+		// notes[1].title = "abc"
+		// noteTitle = ""
+		setNotes([...notes, newNote]);
+		// notes = [newNote, ...notes]
+		setNoteTitle("");
+		// noteTitle = ""
 	};
 
-	const decreaseHandler = () => {
-		setDynamicCounter(dynamicCounter - 1);
+	const removeHandler = (noteId) => {
+		// noteId === "3"
+		// notes.splice()
+		//updatedNotes = [{ id: "1", title: "Note 1" }, { id: "2", title: "Note 2" }]
+		const updatedNotes = notes.filter((note) => note.id !== noteId);
+		//          ({ id: "1", title: "Note 1" }) => "1" !== "3"
+		//          ({ id: "2", title: "Note 2" }) => "2" !== "3"
+		//			({ id: "3", title: "Note 3" })  => "3" !== "3"
+		console.log(updatedNotes);
+
+		setNotes(updatedNotes);
+		// notes = updatedNotes
 	};
 
-	// 1 time
+	// const changeTitleHandler = (event) => {
+	// 	console.log(event.target);
+	// 	// noteTitle = event.target.value;
+	// 	setNoteTitle(event.target.value);
+	// 	// noteTitle = event.target.value;
+	// };
+
 	return (
 		<div className="App">
-			<nav>
-				<li>hello</li>
-				<li>Hello</li>
-			</nav>
-			<p>The value of the counter is {dynamicCounter}</p>
-			<button onClick={increaseHandler}>Increase By 1</button>
-			<button onClick={decreaseHandler}>Decrease By 1</button>
+			<form onSubmit={submitHandler}>
+				<input
+					type="text"
+					value={noteTitle}
+					onChange={(event) => {
+						setNoteTitle(event.target.value);
+						// noteTitle = event.target.value;
+					}}
+				/>
+				<button type="submit">Add Note</button>
+			</form>
+			<ul>
+				{notes.map((note) => (
+					<li className="note-item" key={note.id}>
+						<span>{note.title}</span>
+						<button>Edit</button>
+						<button onClick={() => removeHandler(note.id)}>
+							Delete
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
-// document.getElementsByClassName("App").addEventListener(increaseHandler)
-// p.innerText = counter
+
+// const increaseHandler = (event) => {};
+
+// document.getElementsByClassName("App").addEventListener(increaseHandler);
+// p.innerText = counter;
 
 // section, div
 
