3f392a5a16589a41d2967aa5dbe8a2643015cb37
Asief Mahir <asiefmahir1@gmail.com>
2024-06-03 22:37:31 +0600

    feat: Learn about memo Function and useCallback hook in React

---
+++ src/components/Demo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Demo = () => {
+	console.log("Demo rendering");
+	return <div>hello</div>;
+};
+
+export default Demo;
--- src/pages/CounterApp.jsx
+++ src/pages/CounterApp.jsx
@@ -1,15 +1,41 @@
-import { useCounter } from "../hooks/useCounter";
+import { useState, useCallback } from "react";
+import Title from "../components/Title";
+import Button from "../components/Button";
 
 const CounterApp = () => {
-	const { counter, increaseHandler, decreaseHandler } = useCounter();
+	console.log("Counter app Rendering");
+	const [counter, setCounter] = useState(0);
+	const [counter2, setCounter2] = useState(7);
+	const [counter3, setCounter3] = useState(10);
+
+	const increaseHandler = useCallback(() => {
+		setCounter((currVal) => currVal + 1);
+	}, []); // #555ffj
+
+	const increaseHandler2 = useCallback(() => {
+		setCounter2((currVal) => currVal + 7);
+	}, []); // #8899nn
 	return (
 		<div>
-			<p>The value of the counter is {counter}</p>
-			<button onClick={() => increaseHandler(1)}>Increase By 1</button>
-			<button onClick={() => increaseHandler(5)}>Increase By 5</button>
-
-			<button onClick={() => decreaseHandler(1)}>decrease By 1</button>
-			<button onClick={() => decreaseHandler(3)}>decrease By 3</button>
+			<Title />
+			<div className="counter-app-1">
+				<h3>1st Counter App</h3>
+				<p>The value of the counter is {counter}</p>
+				<Button clickHandler={increaseHandler} />
+			</div>
+			<hr />
+			<div className="counter-app-2">
+				<h3>2nd Counter App</h3>
+				<p>The value of the counter is {counter2}</p>
+				<Button clickHandler={increaseHandler2} />
+			</div>
+			<div className="counter-app-3">
+				<h3>2nd Counter App</h3>
+				<p>The value of the counter is {counter3}</p>
+				<button onClick={() => setCounter3(counter3 + 10)}>
+					Increase By 10
+				</button>
+			</div>
 		</div>
 	);
 };
