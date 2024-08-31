c9f282b7c95024be253d243402518e35f55a9e64
Asief Mahir <asiefmahir1@gmail.com>
2024-08-31 23:02:37 +0600

    Update the api environment again and again

--- next.config.mjs
+++ next.config.mjs
@@ -8,18 +8,18 @@ const nextConfig = {
 			},
 		],
 	},
-	env: {
-		DB_URI: process.env.MONGODB_URI,
-		API: process.env.VERCEL_URL,
-		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-		CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-		CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
-		STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
-		STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-		STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-		DOMAIN: process.env.VERCEL_URL,
-	},
+	// env: {
+	// 	DB_URI: process.env.MONGODB_URI,
+	// 	API: process.env.VERCEL_URL,
+	// 	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+	// 	CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+	// 	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+	// 	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+	// 	STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+	// 	STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+	// 	STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+	// 	DOMAIN: process.env.VERCEL_URL,
+	// },
 	async headers() {
 		return [
 			{
